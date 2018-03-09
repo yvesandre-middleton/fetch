b0bb5225c1cad8e2c1b160d0043c5accb7613193
Willdta <beyondutraining@gmail.com>
2018-03-09 18:15:10 +0000

    dont mind the image

--- js/main.js
+++ js/main.js
@@ -1,47 +1,66 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser', {
+  preload: preload,
+  create: create,
+  update: update
+});
 
 let player
 let bg
 let rec
 let cursors
+let weapon
+let fireButton
 
 function preload() {
   game.load.image('mario', 'assets/mario.png')
   game.load.image('bg2', 'assets/bg2.png')
-  game.load.image('bg', 'assets/bg.png')  
-  game.load.image('rec', 'assets/rec.png')    
+  game.load.image('bg', 'assets/bg.png')
+  game.load.image('rec', 'assets/rec.png')
+  game.load.image('trump', 'assets/trump.png')
 }
 
 function create() {
   // Set World Bounds
-  game.world.setBounds(0,0,1920,1080)
-  
-  // Add Background
-  bg = game.add.sprite(0,0,'bg2')
-  
-  // Add Player
-  player = game.add.sprite(50,50, 'mario')
+  game.world.setBounds(0, 0, 1920, 1080)
 
-  rec = game.add.tileSprite(300,450,400,100, 'bg')
+  // Add Background
+  bg = game.add.sprite(0, 0, 'bg2')
+
+  // Add Weapon
+  weapon = game.add.weapon(30, 'trump')
+
+  weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS
+
+  weapon.bulletSpeed = 600
+
+  weapon.fireRate = 100
+
+  // Add Player
+  player = game.add.sprite(50, 50, 'mario')
+
+  rec = game.add.tileSprite(300, 450, 400, 100, 'bg')
   // rec.body.immovable = true
-  
+
   // Enable physics
   game.physics.enable([player, rec], Phaser.Physics.ARCADE)
-  
+
   // Make sure player can't leave canvas view
   player.body.collideWorldBounds = true
-  
+
   // Sprite Collisions
   rec.body.collideWorldBounds = true
   rec.body.immovable = true
-  
+
   // Have Camera follow
   game.camera.follow(player)
-  
+
   // Position of Character
   player.anchor.setTo(0.5)
 
+  weapon.trackSprite(player, 0, 0, true)
+
   cursors = game.input.keyboard.createCursorKeys()
+  fireButton = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR)
 }
 
 function update() {
@@ -52,17 +71,15 @@ function update() {
 
   if (cursors.left.isDown) {
     player.body.velocity.x = -500;
-  } 
- 
-  else if (cursors.right.isDown) {
+  } else if (cursors.right.isDown) {
     player.body.velocity.x = 500;
-  } 
- 
-  else if (cursors.up.isDown) { 
+  } else if (cursors.up.isDown) {
     player.body.velocity.y = -500;
-  } 
- 
-  else if (cursors.down.isDown) { 
+  } else if (cursors.down.isDown) {
     player.body.velocity.y = 500;
+  }
+
+  if (fireButton.isDown) {
+    weapon.fire()
   }
 }
