8d890b2db52620c5413eb26a48c2325c62285669
Willdta <beyondutraining@gmail.com>
2018-03-10 18:56:39 +0000

    player can teleprt

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ let weapon
 let fireButton
 let enemy
 let tween
+let teleport 
 
 function preload() {
   game.load.image('mario', 'assets/mario.png')
@@ -20,6 +21,7 @@ function preload() {
   game.load.image('rec', 'assets/rec.png')
   game.load.image('head', 'assets/head.png')
   game.load.image('hilary2', 'assets/hilary2.png')
+  game.load.image('nugget', 'assets/nuggets.png')  
 }
 
 function create() {
@@ -45,6 +47,9 @@ function create() {
   enemy = game.add.sprite(400, 200, 'hilary2')
   tween = game.add.tween(enemy)
 
+  // Teleportation Test
+  teleport = game.add.tileSprite(400, 100, 400, 100, 'bg')
+
   // Moving enemy
   tween.to({ x: 700 }, 1000, 'Linear', true, 0, 20, true).loop(true)
        
@@ -52,7 +57,7 @@ function create() {
   rec = game.add.tileSprite(300, 450, 400, 100, 'bg')
 
   // Enable physics   
-  game.physics.enable([player, rec, weapon, enemy], Phaser.Physics.ARCADE)
+  game.physics.enable([player, rec, weapon, enemy, teleport], Phaser.Physics.ARCADE)
 
   // Make sure player can't leave canvas view
   player.body.collideWorldBounds = true
@@ -61,6 +66,8 @@ function create() {
   rec.body.collideWorldBounds = true
   rec.body.immovable = true
 
+  teleport.body.immovable = true
+  
   // Have Camera follow
   game.camera.follow(player)
 
@@ -78,6 +85,7 @@ function update() {
   game.physics.arcade.collide(player, rec)
   game.physics.arcade.collide(player, enemy, killPlayer, null, this)  
   game.physics.arcade.overlap(weapon.bullets, enemy, killEnemy, null, this)
+  game.physics.arcade.collide(player, teleport, teleportPlayer, null, this)
     
   player.body.velocity.x = 0;
   player.body.velocity.y = 0;
@@ -114,4 +122,8 @@ function killPlayer(player, enemy) {
   player.kill()
   player.reset(player.body.velocity.x = 0, player.body.velocity.y = 0)
   console.log('hey')
+}
+
+function teleportPlayer(player, teleport) {
+  player.reset(player.body.velocity.x = 200, player.body.velocity.y = 400)  
 }
